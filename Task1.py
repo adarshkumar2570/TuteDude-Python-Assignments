f24cca17716b0b94d7a1aa2fa846a0d698dc11ff
adarshkumar2570 <adarshkumar2570@gmail.com>
2025-12-11 01:03:49 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print("Addition:",addition)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
